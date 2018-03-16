e09bad601aeea021c6d7e99c7d32de2f872240d4
nhoksun <nhoksun95@gmail.com>
2018-03-16 21:37:57 +0700

    Up
    Copy to Git

---
+++ TowerDefenseTutorial/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int wavepointIndex = 0;
+
+	void Start ()
+	{
+		target = Waypoints.points[0];
+	}
+	void Update ()
+	{
+		Vector3 direction = target.position - transform.position;
+		transform.Translate (direction.normalized * speed * Time.deltaTime, Space.World);
+
+	}
+
+}
