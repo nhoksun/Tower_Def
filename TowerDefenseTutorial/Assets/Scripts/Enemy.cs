f9067199bfb949581492fcc39f45b6f2ed6b9ef4
nhoksun <nhoksun95@gmail.com>
2018-03-18 00:24:38 +0700

    tối chủ nhật
    úp lên để chuyển sang Win làm tiếp

--- TowerDefenseTutorial/Assets/Scripts/Enemy.cs
+++ TowerDefenseTutorial/Assets/Scripts/Enemy.cs
@@ -11,11 +11,26 @@ public class Enemy : MonoBehaviour {
 	{
 		target = Waypoints.points[0];
 	}
+
 	void Update ()
 	{
 		Vector3 direction = target.position - transform.position;
 		transform.Translate (direction.normalized * speed * Time.deltaTime, Space.World);
 
+		if (Vector3.Distance (transform.position, target.position) <= 0.4f) 
+		{
+			GetNextWaypoint();
+		}
 	}
 
+	void GetNextWaypoint()
+	{
+		if (wavepointIndex >= Waypoints.points.Length - 1) 
+		{
+			Destroy (gameObject);
+			return;
+		}
+		wavepointIndex++;
+		target = Waypoints.points [wavepointIndex];
+	}
 }
---
+++ TowerDefenseTutorial/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform enemyPrefab;
+
+	public Transform spawnPoint;
+
+	public float timeBetweenWaves = 5f;
+
+	private float countdownTime = 2f;
+	private int waveIndex = 0 ;
+
+	void Update()
+	{
+		if (countdownTime <= 0f) 
+		{
+			StartCoroutine (SpawnWave ());
+			countdownTime = timeBetweenWaves;
+		}
+		countdownTime -= Time.deltaTime;
+
+	}
+
+	IEnumerator SpawnWave()
+	{
+		waveIndex++;
+		//		Debug.Log ("Wave Incomming!!! ");
+		for (int i = 0; i < waveIndex; i++) 
+		{
+			SpawnEnemy ();
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+//	void SpawnWave()
+//	{
+//		//		Debug.Log ("Wave Incomming!!! ");
+//		for (int i = 0; i < waveIndex; i++) {
+//
+//		}
+//		waveIndex++;
+//	}
+
+	void SpawnEnemy()
+	{
+		Instantiate (enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
