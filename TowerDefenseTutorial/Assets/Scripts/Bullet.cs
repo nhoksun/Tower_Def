e691a6311ac33996fece6e624da907c361e1da67
nhoksunAsus <laihongphat95@gmail.com>
2018-03-27 05:43:27 +0700

    Update
    26-3-2018
    Set Bullet and leanr building

---
+++ TowerDefenseTutorial/Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 70f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        //Target 
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //Move
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+	}
+
+    public void HitTarget()
+    {
+        //Debug.Log("Hit the enemy!!");
+
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+
+    }
+
+}
